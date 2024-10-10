6bc1673f1827dcacef42327783aa787a427941f9
Pushpa Yadav <155540856+PushpaYadav09@users.noreply.github.com>
2024-10-10 22:47:07 +0530

    Add files via upload

---
+++ Arraylist/src/Arraylist.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
+public static void main(String[] args) {
+    ArrayList<String> list=new ArrayList<>();
+    list.add("ajay");
+    list.add("dev");
+    list.add("pooja");
+    list.add("shreya");
+    list.add("rishi");
+    list.add("muskan");
+    System.out.println(list);
+     list.remove(3);
+    System.out.println(list);
+    
+}
